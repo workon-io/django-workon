dec4e5b11393f0ba78c0584a4ba79c03b7c99dec
Damien Autrusseau <autrusseau.damien@gmail.com>
2018-07-29 23:07:21 +0200

    "autofix"

--- workon/urls.py
+++ workon/urls.py
@@ -4,10 +4,13 @@ Django-Select2 URL configuration.
 Add `django_select` to your ``urlconf`` **if** you use any 'Model' fields::
     url(r'^internal/', include('workon.urls')),
 """
-from django.conf.urls import url
+try:
+    from django.urls import path, re_path
+except:
+    from django.conf.urls import url as re_path
 
 from workon.views.select2 import AutoResponseView
 
 urlpatterns = [
-    url(r"^fields/auto.json$", AutoResponseView.as_view(), name="django_select2-json"),
+    re_path(r"^fields/auto.json$", AutoResponseView.as_view(), name="django_select2-json"),
 ]
