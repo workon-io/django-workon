fd616549c973a616bb8ea3e45527015630366c4c
Damien Autrusseau <autrusseau.damien@gmail.com>
2018-08-01 08:15:35 +0200

    "autofix"

--- workon/static/workon/js/workon/active.js
+++ workon/static/workon/js/workon/active.js
@@ -1,4 +1,5 @@
-$(document).on('click', '[data-active]', function() {
+$(document).on('click', '[data-active]', function() 
+{
 	var target = $(this).data('active');
 	if(target) {
 		if(target == "parent") {
@@ -9,4 +10,18 @@ $(document).on('click', '[data-active]', function() {
 		}
 	}
 	(target ? target:$(this)).toggleClass('active');
+});
+
+$(document).on('click', '[data-desactive]', function() 
+{
+	var target = $(this).data('desactive');
+	if(target) {
+		if(target == "parent") {
+			target = $(this).parent()
+		}	
+		else {
+			target = $(target);
+		}
+	}
+	(target ? target:$(this)).removeClass('active');
 });
